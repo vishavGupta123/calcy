8cc6651a2f6f29e00def24c32f53e99fc215dc19
Vishav Gupta <vishavgupta2809@gmail.com>
2020-05-30 09:09:30 -0700

    Calculator project

---
+++ index.js
@@ -0,0 +1,29 @@
+var buttons = document.getElementsByClassName("button");
+var display = document.getElementById("display");
+var operand1 = 0;
+var operand2 = null;
+var operator = null;
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function () {
+    var value = this.getAttribute("data-value");
+    if (value == "+" || value == "-" || value == "/" || value == "*") {
+      operator = value;
+      operand1 = parseFloat(display.textContent);
+      display.innerText = "";
+    } else if (value == "=") {
+      operand2 = parseFloat(display.textContent);
+      var result = eval(operand1 + " " + operator + " " + operand2);
+      display.innerText = result;
+    } else if (value == "AC") {
+      display.innerText = "";
+    } else if (value == "%") {
+      operand1 = parseFloat(display.textContent);
+      operator = "/";
+      operand2 = 100;
+      var result = eval(operand1 + " " + operator + " " + operand2);
+      display.innerText = result;
+    } else {
+      display.innerText += value;
+    }
+  });
+}
